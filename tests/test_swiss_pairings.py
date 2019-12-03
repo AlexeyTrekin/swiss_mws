717706a2260aa46591e53db1f1f5c2d69c3c7bab
trekin <a.trekin@skoltech.ru>
2019-12-03 08:51:27 +0300

    Add new test that reveals the flaw in current pairing method, which puts the same pair twice

--- tests/test_swiss_pairings.py
+++ tests/test_swiss_pairings.py
@@ -5,9 +5,52 @@ from ..tournament.fighter import Fighter
 MAX_FIGHTERS = 50
 
 
+class ProxyFighter:
+    """
+    mocks the basic functionality needed for the fighter class: has name, list of matches, and can be matched with other
+    """
+    def __init__(self, name, hp=12, enemies = []):
+        self.name = name
+        self.hp = hp
+        self.enemies = enemies
+
+    def played(self, other):
+        if other.name in self.enemies:
+            return True
+        else:
+            return False
+
+    def match(self, other, score=0):
+        self.enemies.append(other.name)
+        self.hp += score
+
+
+
 def generate_data_all():
+    """
+    Generates test cases for further tests
+    Will add more test cases.
+    :return:
+    """
     data = []
-    data.append([Fighter(name=str(i+1)) for i in range(12)])
+    data.append([ProxyFighter(name=str(i+1)) for i in range(12)])
+    # This case emerged from our first tournament and revealed that the current method is flawed,
+
+    # As it paired Ryabov with Volodkov again
+    data.append([ProxyFighter('Zakharov', 6, ['Danilov']),
+                 ProxyFighter('Kashitsyn', 7, ['Nekrylov']),
+                 ProxyFighter('Danilov', 7, ['Volodkov', 'Zakharov']),
+                 ProxyFighter('Nekrylov', 7, ['Ryabov', 'Kashitsyn']),
+                 ProxyFighter('Volodkov', 7, ['Ryabov', 'Danilov']),
+                 ProxyFighter('Ryabov', 10, ['Nekrylov', 'Volodkov'])])
+
+    # And reverse HP, as the previous can pass if we sort the fighters
+    data.append([ProxyFighter('Zakharov', 10, ['Danilov']),
+                 ProxyFighter('Kashitsyn', 7, ['Nekrylov']),
+                 ProxyFighter('Danilov', 7, ['Volodkov', 'Zakharov']),
+                 ProxyFighter('Nekrylov', 7, ['Ryabov', 'Kashitsyn']),
+                 ProxyFighter('Volodkov', 7, ['Ryabov', 'Danilov']),
+                 ProxyFighter('Ryabov', 6, ['Nekrylov', 'Volodkov'])])
     # Add more data samples
     return data
 
